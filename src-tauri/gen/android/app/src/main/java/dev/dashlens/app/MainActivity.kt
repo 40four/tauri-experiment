1f3cb1b6b7257b3bad5dd481ff6edfaa1967e6ec
Sean Bryson <seanabryson@protonmail.com>
2026-02-13 21:38:47 -0500

    Initial commit

---
+++ src-tauri/gen/android/app/src/main/java/dev/dashlens/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.dashlens.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
